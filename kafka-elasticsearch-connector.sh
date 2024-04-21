d2ec9ae10c0dcc32e1fe48bf90a4919c8af2ec65
ndaruga <ndaruga.francis@gmail.com>
2024-04-21 23:50:11 +0300

    Elastic search sink

--- kafka-elasticsearch-connector.sh
+++ kafka-elasticsearch-connector.sh
@@ -1,15 +1,15 @@
 curl -X POST http://localhost:8083/connectors -H 'Content-Type: application/json' -d \
 '{
-  "name": "elasticsearch-sink",
+  "name": "elasticsearch_sink_1",
   "config": {
     "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
     "tasks.max": "1",
     "topics": "confirmed_orders",
     "key.ignore": "true",
     "schema.ignore": "true",
-    "connection.url": "http://localhost:9200",
+    "connection.url": "http://elastic:9200",
     "type.name": "_doc",
-    "name": "elasticsearch-sink",
+    "name": "elasticsearch_sink_1",
     "value.converter": "org.apache.kafka.connect.json.JsonConverter",
     "value.converter.schemas.enable": "false"
   }
